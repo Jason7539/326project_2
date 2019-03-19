b8fdf56b937c33e0850097dd07446b90fc00c7e4
Jason Nguyen <jnguyen7539@gmail.com>
2019-03-19 11:16:02 -0700

    cleaned up comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+// Jason Nguyen
+// Chelsea Marfil
+// Sophanna Ek
+// Main.cpp
+// This file servers a a hub to recieve and print out messages
+// produced by probeA.cpp, probeB.cpp, and probeC.cpp. Main.cpp is also
+// responsible for terminating probeB after 10,000 messages have been recieved.
+// The message queue is also created and deleted in main.cpp
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -10,11 +18,6 @@
 #include <errno.h>
 #include "force_patch.h"
 
-/*
- * This files executes the datahub which takes input from
- * Probe A, Probe B, and Probe C. It then ouputs the Process Id of the probe
- * and the data that the probe sent.
-*/
 using namespace std;
 
 int main()
@@ -25,27 +28,18 @@ int main()
 	};
 
     int qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0606);   // Create the fresh message queue
-    if(qid == -1){                             // Exit out of program if the message queue is not succesful
+    if(qid == -1){                                              // Exit out of program if the message queue is not succesful
         cout << strerror(errno) << endl;
         return -1;
     }
 
-    /////////////////////////////
     // changing the number of bytes allowed in the message queue
     struct msqid_ds msgds;
     msgctl(qid, IPC_STAT, &msgds);          // getting the info of the message queue
-
-    // cout << "bytes of msgQ:" << (int) msgds.msg_qbytes << endl;
     cout << "The size of the queue is: " << msgds.msg_qbytes << endl;
-
     msgds.msg_qbytes = 4194304;
-
     msgctl(qid, IPC_SET, &msgds);           // increase the size of the message queue
-
     cout << "The size of the queue is now : " << msgds.msg_qbytes << endl;
-    /////////////////////////////////
-
-
 
     // while any of the probes are on keep checking the messages
     bool flag_a = true;
@@ -62,23 +56,16 @@ int main()
     string Bpid;
     string Cpid;
 
-    // constants to hold the qid of each probe messages
-    const int Amtype = 111;
-    const int Bmtype = 112;
-    const int Cmtype = 113;
-
     buf msg;        // declare container for the message
-    int size = sizeof(msg)-sizeof(long);         // calculate the size of the message.
+    int size = sizeof(msg)-sizeof(long);// calculate the size of the message.
 
-    int pass = 0;       // variable to track whether a msgrcv is succesful
+    int pass = 0;                       // variable to track whether a msgrcv is succesful
 
     int Bcounter = 0;                   // holds numbers of msg received. terminate B after 10,000 messages
 
-  //  char ack[];
-
-    while(flag_a || flag_b || flag_c)             // keeps checking messages while probeA and probeB are still active
+    while(flag_a || flag_b || flag_c)   // keep checking message while probes are on
     {
-      pass = msgrcv(qid, (struct msgbuf *)&msg, size, -113, 0); // reading the messages of probeA, probeB, or probeC
+      pass = msgrcv(qid, (struct msgbuf *)&msg, size, -113, 0); // recieve message from A, B or C
 
       cout << "mtype: " << msg.mtype << "and the pass is " << pass << endl;
       if(pass != -1){               // process messsage if it msgrcv succeeds
@@ -87,8 +74,7 @@ int main()
 
         if(Bcounter == 10000){      // terminate b when we recieve the 10000 msg
           cout << "REACHED THE END OF B ********************************************************************" << endl;
-          cout << Bpid << "the b id is " << endl;
-          int id = stoi(Bpid);        // get the pid of B
+          int id = stoi(Bpid);                      // get the pid of B
           force_patch(id);                          // send the signal the end B
           flag_b = false;                           // set probeB as off
         }
@@ -97,16 +83,16 @@ int main()
         {
           case 109: // recieved a goodbye message from probeC
             cout << "ended C " << endl;
-            flag_c = false;
+            flag_c = false;       // Turn C flag off
             break;
           case 110: // recieved a goodbye message from probeA
             cout << "ended A " << endl;
             flag_a = false;
             break;
-          case 111:                   // recieving probeA message
+          case 111: // recieve probeA message
             if(firstA == false){      // store the pid if it's the first message
               Apid = msg.greeting;
-              firstA = true;         // no process message from A that aren't the pid
+              firstA = true;
             }
             else{   // Process ProbeA message that are not the first
               cout << "from A " << Apid << ": " << msg.greeting << endl;
@@ -116,31 +102,22 @@ int main()
             // sending acknowledge to A
             msg.mtype = 114;
             strcpy(msg.greeting, "acknowledge");
-            if(msgsnd(qid, (struct msgbuf *)&msg, size, IPC_NOWAIT) == -1){  // sending  acknowledge mesg to A
-              perror("sending");
-              cout << errno << endl;
-              cout << strerror(errno) << endl;
+            msgsnd(qid, (struct msgbuf *)&msg, size, 0);  // sending  acknowledge mesg to A
 
-              msgctl(qid, IPC_STAT, &msgds);          // getting the info of the message queue
-              cout << "message size is "  << msgds.msg_qbytes << endl;
-
-              cin >> firstA;
-            }
             cout << "sent acknowledge " << endl;
             break;
 
-          case 112:
-
+          case 112: // recieve probeB message
             if(firstB == false){        // process the pid of B if it's the 1st msg
               Bpid = msg.greeting;      // save the pid
               firstB = true;
             }
-            else{                       // prints B's messages that are not the first
+            else{ // prints B's messages that are not the first
               cout << "from B " << Bpid << ": " << msg.greeting << endl;
             }
             break;
 
-          case 113:                     // process message from probeC
+          case 113: // process message from probeC
             char PID [10];
             char message [10];
             // Gets the PID from msg.greeting which is separated from the reading by delimiter " "
@@ -157,21 +134,20 @@ int main()
             }
             break;
           default:
-            // datahub read a message that it wasn't suppose to( sent from itself)
+            // datahub read a message that it wasn't suppose to
             cout << "got a wierd one type:" << msg.mtype << "mesg:" << msg.greeting << endl;
-            cin >> msg.greeting;
-            pass = msgsnd(qid, (struct msgbuf *)&msg, size, 0);  // place the read message back
+            return -1;
             break;
         }
       }
       else{
         printf("got an error \n");
+        return -1;
         break;
       }
     }
-    // Close Probe C after probe A and B have ended
 
-    msgctl (qid, IPC_RMID, NULL);                           // deallocate the queue.
+    msgctl (qid, IPC_RMID, NULL); // deallocate the queue.
     cout << "pogram ended " << endl;
     return 0;
 }
